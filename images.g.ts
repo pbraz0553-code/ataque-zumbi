691b0041b98ea85222694de8cb8a5324fdeec299
pbraz0553-code <pbraz0553@gmail.com>
2026-01-13 21:25:50 -0300

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,75 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image3":
+            case "myImage1":return img`
+. . f f f f . . 
+. f 1 1 1 1 f . 
+f 1 1 1 1 1 1 f 
+f 1 1 f 1 1 f f 
+f 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 f 
+. f f f f f f . 
+`;
+            case "image4":
+            case "myImage2":return img`
+. . f f f f . . 
+. f 1 1 1 1 f . 
+f 1 1 1 1 1 1 f 
+f 1 f 1 1 f 1 f 
+f 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 f 
+. f f f f f f . 
+`;
+            case "image2":
+            case "myImage0":return img`
+. . f f f f . . 
+. f 1 1 1 1 f . 
+f 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 f 
+f 1 f 1 1 f 1 f 
+f 1 1 1 1 1 1 f 
+. f f f f f f . 
+`;
+            case "image5":
+            case "myImage3":return img`
+. . f f f f . . 
+. f 1 1 1 1 f . 
+f 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 f 
+f 1 1 1 1 1 1 f 
+. f f f f f f . 
+`;
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,125 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . b d b c . . . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . d d 5 1 1 1 1 1 1 1 5 d d . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . . . c b d b c . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, heroi, Render.getAttribute(Render.attribute.dirX) * 55, Render.getAttribute(Render.attribute.dirY) * 55)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Render.jumpWithHeightAndDuration(heroi, 16, 750)
+})
+let projectile: Sprite = null
+let heroi: Sprite = null
+heroi = Render.getRenderSpriteVariable()
+tiles.setCurrentTilemap(tilemap`level0`)
+scene.setBackgroundColor(6)
+Render.moveWithController(2, 4, 1)
+let Mira = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 2 . . . . . . . . 
+    . . . . . . . 2 . . . . . . . . 
+    . . . . . 2 2 2 2 2 . . . . . . 
+    . . . . . . . 2 . . . . . . . . 
+    . . . . . . . 2 . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+Mira.setFlag(SpriteFlag.RelativeToCamera, true)
+let Arma = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+Arma.setFlag(SpriteFlag.RelativeToCamera, true)
+let EnergyBar = statusbars.create(60, 4, StatusBarKind.Health)
+EnergyBar.setFlag(SpriteFlag.RelativeToCamera, true)
+EnergyBar.setPosition(38, 113)
+let Zumbi = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ......fd11111111df......
+    ......fddd1111dddf......
+    ......fbdbfddfbdbf......
+    ......fcdcf11fcdcf......
+    .......fb111111ffff.....
+    ......fffcdb1bc111cf....
+    ....fc111cbfbf1b1b1f....
+    ....f1b1b1ffffbfbfbf....
+    ....fbfbfffffff.........
+    .........fffff..........
+    ..........fff...........
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Enemy)
+tiles.placeOnTile(Zumbi, tiles.getTileLocation(20, 4))
+game.onUpdateInterval(500, function () {
+    characterAnimations.loopFrames(
+    Arma,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    100,
+    characterAnimations.rule(Predicate.MovingUp, Predicate.MovingDown)
+    )
+})
+game.onUpdateInterval(500, function () {
+    scene.followPath(Zumbi, scene.aStar(tiles.locationOfSprite(Zumbi), tiles.locationOfSprite(heroi)))
+})
